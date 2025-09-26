56c67be3bb7e62f74540daa00d78b5d27049cc5d
Daniel Lima <137017200+DanielGLima@users.noreply.github.com>
2025-09-26 03:29:07 -0600

    Creacion de Proyecto

---
+++ src/main/java/gt/edu/miumg/sistema_gestion_de_sesiones_de_bienestar/SistemaGestionDeSesionesDeBienestarApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.miumg.sistema_gestion_de_sesiones_de_bienestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionDeSesionesDeBienestarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGestionDeSesionesDeBienestarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gt/edu/miumg/sistema_gestion_de_sesiones_de_bienestar/SistemaGestionDeSesionesDeBienestarApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.miumg.sistema_gestion_de_sesiones_de_bienestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionDeSesionesDeBienestarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
